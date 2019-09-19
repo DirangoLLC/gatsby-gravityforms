6521d0e783796301c19f83e67763fac197284012
Ben-Kincaid <benk@dirango.com>
2019-09-18 18:56:18 -0700

    Adding initial logic for sourcing from wordpress + gatsby, and including demo netlify lambda functions for handle sending of GF data to wordpress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,70 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import GravityFormForm from "gatsby-gravityforms-component"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const { allGfForm } = useStaticQuery(
+    graphql`
+      query {
+        allGfForm {
+          edges {
+            node {
+              formId
+              slug
+              apiURL
+              descriptionPlacement
+              formFields {
+                id
+                label
+                description
+                descriptionPlacement
+                type
+                choices
+                content
+                errorMessage
+                inputMaskValue
+                isRequired
+                visibility
+                cssClass
+                placeholder
+                size
+                defaultValue
+                maxLength
+              }
+              button {
+                text
+              }
+              confirmations {
+                message
+              }
+            }
+          }
+        }
+      }
+    `
+  )
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+        <GravityFormForm
+          id={2}
+          formData={allGfForm}
+          presetValues={{ input_1: "special_value" }}
+          lambda={"http://localhost:9000/sendGfFormData"}
+        />
+      </div>
+      <Link to="/page-2/">Go to page 2</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
